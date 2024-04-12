74aad91d5905e0a76652100a8cc7c8111fab48d4
이재윤 <154980427+JYL35@users.noreply.github.com>
2024-04-12 18:09:01 +0900

    [Silver II] Title: 이항 계수 2, Time: 144 ms, Memory: 18264 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -10,7 +10,7 @@ public class Main {
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= i; j++) {
 				if(j == 0 || j == i) dp[i][j] = 1;
-				else dp[i][j] = (dp[i-1][j] % 10007 + dp[i-1][j-1] % 10007) % 10007;
+				else dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % 10007;
 				
 				if(i == n && j == k) return dp[n][k];
 			}
