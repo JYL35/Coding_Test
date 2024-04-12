853077ea199e6a46c2f52103415743fa5fb97e3e
이재윤 <154980427+JYL35@users.noreply.github.com>
2024-04-12 18:21:58 +0900

    [Silver II] Title: 이항 계수 2, Time: 140 ms, Memory: 18216 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -1,31 +1,27 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-
 public class Main {
+	static int[][] dp;
+	static final int div = 10007;
 	
 	private static int binomial(int n, int k) {
-		int[][] dp = new int[n+1][n+1];
+		if (dp[n][k] > 0) return dp[n][k];
 		
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= i; j++) {
-				if(j == 0 || j == i) dp[i][j] = 1;
-				else dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % 10007;
-				
-				if(i == n && j == k) return dp[n][k];
-			}
-		}
-		return 0;
+		if (k == 0 || k == n) return dp[n][k] = 1;
+		
+		return dp[n][k] = (binomial(n-1, k) + binomial(n-1, k-1)) % div;
 	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
+		dp = new int[n+1][n+1];
+		
 		System.out.println(binomial(n, k));
-	}
-	
+    }
 }
