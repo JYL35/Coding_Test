8a6078c12b9e2306e1408148ed5a39393db46b1f
Jaeyoun Lee <154980427+JYL35@users.noreply.github.com>
2024-07-03 20:51:14 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 524 ms, Memory: 124220 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static boolean[] visit;
+    static boolean[][] graph;
+
+    private static int countComponent() {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if(visit[i]) continue;
+            Stack<Integer> stack = new Stack<>();
+            stack.push(i);
+            while(!stack.isEmpty()){
+                int u = stack.pop();
+                if(!visit[u]){
+                    visit[u] = true;
+                    for (int v = 1; v <= n; v++) {
+                        if(graph[u][v] && !visit[v]) {
+                            stack.push(v);
+                        }
+                    }
+                }
+            }
+            count++;
+        }
+
+
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[n+1];
+        graph = new boolean[n+1][n+1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u][v] = true;
+            graph[v][u] = true;
+        }
+
+        System.out.println(countComponent());
+    }
+}
