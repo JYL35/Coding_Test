d6b5946fdd4cc5c7670908d90572afd11a931c6b
Jaeyoun Lee <154980427+JYL35@users.noreply.github.com>
2024-07-29 21:37:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    static int n, complexNum;
+    static int[][] map;
+    static boolean[][] visit;
+    static final int[][] dirArr = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private static int[] BFS() {
+        Queue<int[]> queue = new LinkedList<>();
+        Queue<Integer> resultQ = new LinkedList<>();
+        complexNum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(map[i][j] == 1 && !visit[i][j]) {
+                    int houseCnt = 0;
+                    queue.add(new int[]{i, j});
+                    visit[i][j] = true;
+                    while(!queue.isEmpty()) {
+                        houseCnt++;
+                        int[] temp = queue.poll();
+                        int x = temp[0];
+                        int y = temp[1];
+                        for (int k = 0; k < 4; k++) {
+                            int nextX = x + dirArr[k][0];
+                            int nextY = y + dirArr[k][1];
+
+                            if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= n) continue;
+                            if(visit[nextX][nextY] || map[nextX][nextY] == 0) continue;
+
+                            queue.add(new int[]{nextX, nextY});
+                            visit[nextX][nextY] = true;
+                        }
+                    }
+                    resultQ.add(houseCnt);
+                    complexNum++;
+                }
+            }
+        }
+
+        int[] arr = new int[resultQ.size()];
+        for (int i = 0; !resultQ.isEmpty(); i++) {
+            arr[i] = resultQ.poll();
+        }
+        Arrays.sort(arr);
+
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visit = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int[] result = BFS();
+
+        System.out.println(complexNum);
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+}
